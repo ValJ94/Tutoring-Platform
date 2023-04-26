22b51ede3dad09c8af52e1142003c6ca50b3f07a
Valborg Johannesen <2328244j@student.gla.ac.uk>
2023-04-26 22:12:26 +0100

    base HTML template, some models

--- tutoring_platform/tutoring/models.py
+++ tutoring_platform/tutoring/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from user import models as usermodels
 
 # Create your models here.
+
+
+maxCharLength = 128
+
+
+class TeachingSubject(models.Model):
+    title = models.CharField(max_length=maxCharLength)
+    subject = models.CharField(max_length=maxCharLength)
+    teacher = models.ForeignKey(usermodels.User, on_delete=models.CASCADE, related_name='+')
+    DIFFICULTY_CHOICES = (
+        ('B', 'Beginner'),
+        ('I', 'Intermediate'),
+        ('E', 'Expert'),
+    )
+    difficulty_level = models.CharField(max_length=3, choices=DIFFICULTY_CHOICES)
+
+    def __str__(self):
+        return self.title + " taught by " + self.teacher
+
+
+class MessageThread(models.Model):
+    user = models.ForeignKey(usermodels.User, on_delete=models.CASCADE, related_name='+')
+    receiver = models.ForeignKey(usermodels.User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.first_name
+
+class Message(models.Model):
+    messageThread = models.ForeignKey('MessageThread', on_delete=models.CASCADE, blank = True, related_name='+')
+    messageSender = models.ForeignKey(usermodels.User, on_delete=models.CASCADE, related_name='+')
+    messageReceiver = models.ForeignKey(usermodels.User, on_delete=models.CASCADE, related_name='+')
+    content = models.TextField(max_length=1000)
+    date = models.DateTimeField(default=timezone.now)
+    messageRead = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "from: " + self.messageSender.username + " to:" + self.messageReceiver.username + " messageRead: " + str(self.messageRead)
--- tutoring_platform/user/models.py
+++ tutoring_platform/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+maxCharLength = 128
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=maxCharLength)
+    middle_name = models.CharField(max_length=maxCharLength, blank=True)
+    surname = models.CharField(max_length=maxCharLength)
+    age = models.IntegerField()
+    description = models.TextField(blank=True)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('NB', 'Non Binary'),
+        ('PNTD', 'Prefer not to say'),
+    )
+    gender = models.CharField(max_length=4, choices=GENDER_CHOICES, blank=True)
+    picture = models.ImageField(upload_to='profilePics', blank=True)
+
+
+    def __str__(self):
+        return self.user.first_name + self.user.last_name
